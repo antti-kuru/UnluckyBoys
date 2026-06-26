fca8cac8877825e92c70607196e19b2ea30b4909
Antti Kuru <antti.i.kuru@gmail.com>
2026-06-26 12:57:15 +0300

    Updated Sweaty's position, nickname, and sportsgamer url

---
+++ database-migrations/V010__update_sweaty.sql
@@ -0,0 +1,6 @@
+update players
+set position = 'RW',
+    nickname = 'sweatyyy-99',
+    sportsgamer_url = 'https://sportsgamer.gg/players/914',
+    updated_at = datetime('now')
+where slug = 'j-arjalainen';
